06dbc6a5d6d9c8eda42ef1903a47e727fd23cbc4
Ethan <ethan1.hernou@epitech.eu>
2023-11-30 11:31:02 +0100

    Compilation (./): add Asio to the 3rd-party cmake installer

--- Server/src/Main.cpp
+++ Server/src/Main.cpp
@@ -6,12 +6,45 @@
 */
 
 #include <iostream>
+#include <asio.hpp>
 
 int main(int ac, char **av)
 {
     (void)ac;
     (void)av;
 
-    std::cout << "Hello World !" << std::endl;
-    return 0;
+    std::cout << "Asio World !" << std::endl;
+
+    try {
+        asio::io_context io_context;
+
+        // Création du socket accepteur
+        asio::ip::tcp::acceptor acceptor(io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 8080));
+
+        std::cout << "Server started. Listening on port 8080...\n";
+
+        while (true) {
+            asio::ip::tcp::socket socket(io_context);
+
+            // En attente et acceptation d'une nouvelle connexion
+            acceptor.accept(socket);
+
+            std::cout << "New connection established: " << socket.remote_endpoint() << std::endl;
+
+            // Lecture et renvoi des données reçues
+            char data[1024];
+            size_t len = socket.read_some(asio::buffer(data, sizeof(data)));
+
+            asio::error_code error;
+            asio::write(socket, asio::buffer(data, len), error);
+
+            if (error) {
+                std::cerr << "Error: " << error.message() << std::endl;
+            }
+
+            socket.shutdown(asio::ip::tcp::socket::shutdown_both);
+        }
+    } catch (std::exception &e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
 }
