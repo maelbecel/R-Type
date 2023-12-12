5ae78a5279528afafd8f0eeeaea2f179d8fc7066
ZeNo <enzo.garnier@epitech.eu>
2023-12-12 15:43:42 +0100

    build (Client/): fix double inclusion

--- R-Type/Entity/CreateBackground.hpp
+++ R-Type/Entity/CreateBackground.hpp
@@ -17,16 +17,7 @@ namespace Exodia {
      *
      * @param _World
      */
-    void CreateBackground(World *_World) {
-        // Create background
-        Entity *background = _World->CreateEntity("Background");
-
-        background->AddComponent<IDComponent>();
-        background->AddComponent<CircleRendererComponent>(glm::vec4{ 0.0f, 0.0f, 0.005f, 1.0f });
-        background->GetComponent<TransformComponent>().Get().Scale.y = 100.0f;
-        background->GetComponent<TransformComponent>().Get().Scale.x = 100.0f;
-        background->GetComponent<TransformComponent>().Get().Translation.z = -0.5f;
-    };
+    void CreateBackground(World *_World);
 };
 
 #endif /* !CREATEBACKGROUND_HPP_ */
