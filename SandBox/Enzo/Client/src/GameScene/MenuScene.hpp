9ed66b286c43575934a311a333c490cb243d3023
ZeNo <enzo.garnier@epitech.eu>
2023-12-27 16:11:08 +0100

    feat (SandBox/Enzo/): add scene from Ethan

---
+++ SandBox/Enzo/Client/src/GameScene/MenuScene.hpp
@@ -0,0 +1,56 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Type
+** File description:
+** MenuScene
+*/
+
+#ifndef MENUSCENE_HPP_
+    #define MENUSCENE_HPP_
+
+    // Exodia includes
+    #include "Exodia.hpp"
+
+    // R-Type includes
+    #include "GameScene/GameScene.hpp"
+
+namespace RType {
+
+    class MenuScene : public GameScene {
+
+        //////////////////////////////
+        // Constructor & Destructor //
+        //////////////////////////////
+        public:
+
+            MenuScene() = default;
+            ~MenuScene() override = default;
+
+        /////////////
+        // Methods //
+        /////////////
+        public:
+
+            void OnCreate() override;
+            void OnDestroy() override;
+
+            void OnUpdate(Exodia::Timestep ts) override;
+            void OnEvent(Exodia::Event &event) override;
+
+        ////////////
+        // Events //
+        ////////////
+        private:
+
+            bool OnKeyPressedEvent(Exodia::KeyPressedEvent &event);
+
+        ////////////////
+        // Attributes //
+        ////////////////
+        private:
+
+            Ref<Exodia::Scene> _Scene;
+    };
+};
+
+#endif /* !MENUSCENE_HPP_ */
